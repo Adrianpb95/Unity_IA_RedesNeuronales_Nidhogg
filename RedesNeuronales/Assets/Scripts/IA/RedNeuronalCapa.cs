3b9d76f38fdee0746aca59199721b99d30ea83d0
unknown <adrianpb95@gmail.com>
2019-02-21 20:59:20 +0100

    .

---
+++ RedesNeuronales/Assets/Scripts/IA/RedNeuronalCapa.cs
@@ -0,0 +1,201 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedNeuronalCapa
+{
+    //Valores
+    public int numeroDeNodos, numeroDeNodosHijo, numeroDeNodosPadre;
+    public float[,] pesos;
+    public float[,] pesosIncremento;
+    public float[] valoresNeuronas;
+    public float[] valoresDeseados;
+    public float[] errores;
+    public float[] biasValores;
+    public float[] biasPesos;
+
+    public RedNeuronalCapa capaPadre, capaHija;
+
+    //Constructor de capa
+    public RedNeuronalCapa(int nNodosPadre, int nNodos, int nNodosHijo)
+    {
+        numeroDeNodos = nNodos;
+        numeroDeNodosPadre = nNodosPadre;
+        numeroDeNodosHijo = nNodosHijo;
+        capaPadre = null;
+        capaHija = null;
+    }
+
+    //Dependiendo de los valores recibidos, te ordena las capa
+    public void Inicializar(int nNodos, RedNeuronalCapa padre, RedNeuronalCapa hija)
+    {
+        numeroDeNodos = nNodos;
+
+        valoresNeuronas = new float[nNodos];
+        valoresDeseados = new float[nNodos];
+        errores = new float[nNodos];
+
+        if (padre != null)
+        {
+            capaPadre = padre;
+            numeroDeNodosPadre = padre.numeroDeNodos;
+        }
+        else
+        {
+            capaPadre = null;
+            numeroDeNodosPadre = 0;
+        }
+
+        if (hija != null)
+        {
+            capaHija = hija;
+            numeroDeNodosHijo = hija.numeroDeNodos;
+            pesos = new float[numeroDeNodos, numeroDeNodosHijo];
+            pesosIncremento = new float[numeroDeNodos, numeroDeNodosHijo];
+            biasValores = new float[numeroDeNodosHijo];
+            biasPesos = new float[numeroDeNodosHijo];
+        }
+        else
+        {
+            capaHija = null;
+            numeroDeNodosHijo = 0;
+            pesos = null;
+            pesosIncremento = null;
+            biasValores = null;
+            biasPesos = null;
+        }
+
+        for (int i = 0; i < numeroDeNodos; i++)
+        {
+            valoresNeuronas[i] = 0;
+            valoresDeseados[i] = 0;
+            errores[i] = 0;
+        }
+
+        if (hija != null)
+        {
+            for (int i = 0; i < numeroDeNodos; i++)
+            {
+                for (int j = 0; j < numeroDeNodosHijo; j++)
+                {
+                    pesos[i, j] = 0;
+                    pesosIncremento[i, j] = 0;
+                }
+            }
+
+            for (int j = 0; j < numeroDeNodosHijo; j++)
+            {
+                biasValores[j] = -1;
+                biasPesos[j] = 0;
+            }
+        }
+    }
+
+
+    //Guarda pesos aleatorios entre -1 y 1
+    public void AsignarPesosAleatorios()
+    {
+        if (capaHija != null)
+        {
+            for (int i = 0; i < numeroDeNodos; i++)
+            {
+                for (int j = 0; j < numeroDeNodosHijo; j++)
+                {
+                    pesos[i, j] = Random.Range(-1f, 1f);
+                }
+            }
+
+            for (int j = 0; j < numeroDeNodosHijo; j++)
+            {
+                biasPesos[j] = Random.Range(-1f, 1f);
+            }
+        }
+    }
+
+    //En funcion de los valores de la capa padre, calcula los valores de cada neurona
+    public void CalcularValoresNeuronas()
+    {
+        if (capaPadre != null)
+        {
+            for (int j = 0; j < numeroDeNodos; j++)
+            {
+                float x = 0;
+                for (int i = 0; i < numeroDeNodosPadre; i++)
+                {
+                    x += capaPadre.valoresNeuronas[i] * capaPadre.pesos[i, j];
+                }
+                x += capaPadre.biasValores[j] * capaPadre.biasPesos[j];
+
+                if ((capaHija == null) && Constantes.OUTPUT_LINEAL)
+                {
+                    valoresNeuronas[j] = x;
+                }
+                else
+                {
+                    valoresNeuronas[j] = 1.0f / (1 + Mathf.Exp(-x)); // Función logística
+                }
+            }
+        }
+    }
+
+    //Calcula el error --> diferencia entre el valor obtenido y el deseado de cada capa
+    public void CalcularErrores()
+    {
+        if (capaHija == null)
+        {
+            for (int i = 0; i < numeroDeNodos; i++)
+            {
+                // Ecuación del error
+                errores[i] = (valoresDeseados[i] - valoresNeuronas[i]) * valoresNeuronas[i] * (1.0f - valoresNeuronas[i]);
+            }
+        }
+        else if (capaPadre == null)
+        {
+            for (int i = 0; i < numeroDeNodos; i++)
+            {
+                errores[i] = 0.0f;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < numeroDeNodos; i++)
+            {
+                float suma = 0;
+                for (int j = 0; j < numeroDeNodosHijo; j++)
+                {
+                    suma += capaHija.errores[j] * pesos[i, j];
+                }
+                errores[i] = suma * valoresNeuronas[i] * (1.0f - valoresNeuronas[i]);
+            }
+        }
+    }
+
+    //Se reajustan los pesos segun el aprendizaje, sin aleatoriedad
+    public void AjustarPesos()
+    {
+        if (capaHija != null)
+        {
+            for (int i = 0; i < numeroDeNodos; i++)
+            {
+                for (int j = 0; j < numeroDeNodosHijo; j++)
+                {
+                    float dw = Constantes.RATIO_APRENDIZAJE * capaHija.errores[j] * valoresNeuronas[i];
+                    if (Constantes.USO_INERCIA)
+                    {
+                        pesos[i, j] += dw + Constantes.RATIO_INERCIA * pesosIncremento[i, j];
+                        pesosIncremento[i, j] = dw;
+                    }
+                    else
+                    {
+                        pesos[i, j] += dw;
+                    }
+                }
+            }
+            for (int j = 0; j < numeroDeNodosHijo; j++)
+            {
+                float dw = Constantes.RATIO_APRENDIZAJE * capaHija.errores[j] * biasValores[j];
+                biasPesos[j] += dw;
+            }
+        }
+    }
+}
---
+++ RedesNeuronales/Assets/Scripts/IA/RedNeuronalCharacter.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RedNeuronalCharacter : MonoBehaviour
+{
+    private RedNeuronal red;
+    public int numInput = 3, numHidden = 6, numOutput = 6;
+    
+    //INPUTS 
+    //  0 =distancia   ;   1=estadoEspada  ;   2=pegando
+    // OUTPUT:
+    // Attack ; MoveTo ; MoveAway ; Stay ; SwordUp ; SwordDown ; Jump
+
+private static float[,] entrenamiento =
+            {
+                //Fomenta el avance
+                { 6.0f, 1.0f, 0.0f,             0.1f, 0.9f, 0.1f, 0.4f, 0.2f, 0.2f, 0.1f },
+                { 4.0f, 1.0f, 0.0f,             0.1f, 0.9f, 0.1f, 0.4f, 0.2f, 0.2f, 0.1f },
+                { 3.0f, 0.0f, 0.0f,             0.6f, 0.6f, 0.4f, 0.2f, 0.4f, 0.2f, 0.1f },
+                { 3.0f, 1.0f, 0.0f,             0.6f, 0.6f, 0.2f, 0.2f, 0.4f, 0.4f, 0.1f },
+                { 2.5f, 2.0f, 0.0f,             0.6f, 0.7f, 0.3f, 0.2f, 0.3f, 0.4f, 0.1f },
+                { 4.0f, 0.0f, 0.0f,             0.7f, 0.7f, 0.1f, 0.1f, 0.8f, 0.4f, 0.1f },
+                { 4.0f, 2.0f, 0.0f,             0.7f, 0.7f, 0.1f, 0.1f, 0.1f, 0.9f, 0.1f },
+                { 9.0f, 1.0f, 0.0f,             0.1f, 0.9f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f },
+                { 3.0f, 0.0f, 0.0f,             0.5f, 0.7f, 0.1f, 0.1f, 0.4f, 0.7f, 0.1f },
+                { 3.0f, 1.0f, 0.0f,             0.5f, 0.1f, 0.1f, 0.1f, 0.5f, 0.5f, 0.1f },
+                { 3.0f, 2.0f, 0.0f,             0.5f, 0.7f, 0.1f, 0.1f, 0.7f, 0.1f, 0.1f },
+                { 2.5f, 0.0f, 0.0f,             0.8f, 0.8f, 0.5f, 0.1f, 0.2f, 0.2f, 0.1f },
+                { 2.5f, 1.0f, 0.0f,             0.8f, 0.8f, 0.5f, 0.1f, 0.2f, 0.2f, 0.1f },
+                { 2.5f, 2.0f, 0.0f,             0.8f, 0.8f, 0.5f, 0.1f, 0.2f, 0.2f, 0.1f },
+                { 3.0f, 0.0f, 1.0f,             0.1f, 0.7f, 0.1f, 0.1f, 0.7f, 0.1f, 0.1f },
+                { 3.0f, 2.0f, 1.0f,             0.1f, 0.7f, 0.1f, 0.1f, 0.1f, 0.7f, 0.1f },
+                { 3.5f, 1.0f, 1.0f,             0.1f, 0.7f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f },
+                { 3.5f, 0.0f, 1.0f,             0.7f, 0.7f, 0.2f, 0.1f, 0.7f, 0.1f, 0.1f },
+                { 3.5f, 2.0f, 1.0f,             0.7f, 0.5f, 0.5f, 0.1f, 0.1f, 0.7f, 0.1f },
+
+                //Fomenta el ataque
+                /*
+                { 6.0f, 1.0f, 0.0f,             0.7f, 0.9f, 0.1f, 0.4f, 0.2f, 0.2f, 0.1f },
+                { 4.0f, 1.0f, 0.0f,             0.7f, 0.9f, 0.1f, 0.4f, 0.2f, 0.2f, 0.1f },
+                { 3.0f, 0.0f, 0.0f,             0.7f, 0.6f, 0.4f, 0.2f, 0.4f, 0.2f, 0.1f },
+                { 3.0f, 1.0f, 0.0f,             0.7f, 0.6f, 0.2f, 0.2f, 0.4f, 0.4f, 0.1f },
+                { 2.5f, 2.0f, 0.0f,             0.7f, 0.7f, 0.3f, 0.2f, 0.3f, 0.4f, 0.1f },
+                { 4.0f, 0.0f, 0.0f,             0.7f, 0.7f, 0.1f, 0.1f, 0.8f, 0.4f, 0.1f },
+                { 4.0f, 2.0f, 0.0f,             0.7f, 0.7f, 0.1f, 0.1f, 0.1f, 0.9f, 0.1f },
+                { 9.0f, 1.0f, 0.0f,             0.7f, 0.9f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f },
+                { 3.0f, 0.0f, 0.0f,             0.5f, 0.7f, 0.1f, 0.1f, 0.4f, 0.7f, 0.1f },
+                { 3.0f, 1.0f, 0.0f,             0.5f, 0.1f, 0.1f, 0.1f, 0.5f, 0.5f, 0.1f },
+                { 3.0f, 2.0f, 0.0f,             0.5f, 0.7f, 0.1f, 0.1f, 0.7f, 0.1f, 0.1f },
+                { 2.5f, 0.0f, 0.0f,             0.8f, 0.8f, 0.5f, 0.1f, 0.2f, 0.2f, 0.1f },
+                { 2.5f, 1.0f, 0.0f,             0.8f, 0.8f, 0.5f, 0.1f, 0.2f, 0.2f, 0.1f },
+                { 2.5f, 2.0f, 0.0f,             0.8f, 0.8f, 0.5f, 0.1f, 0.2f, 0.2f, 0.1f },
+                { 3.0f, 0.0f, 1.0f,             0.6f, 0.7f, 0.1f, 0.1f, 0.7f, 0.1f, 0.1f },
+                { 3.0f, 2.0f, 1.0f,             0.6f, 0.7f, 0.1f, 0.1f, 0.1f, 0.7f, 0.1f },
+                { 3.5f, 1.0f, 1.0f,             0.6f, 0.7f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f },
+                { 3.5f, 0.0f, 1.0f,             0.7f, 0.7f, 0.2f, 0.1f, 0.7f, 0.1f, 0.1f },
+                { 3.5f, 2.0f, 1.0f,             0.7f, 0.5f, 0.5f, 0.1f, 0.1f, 0.7f, 0.1f },
+                */
+        };
+
+    public static RedNeuronalCharacter instance;
+
+    //Inicializacion
+    void Start()
+    {
+        instance = this;
+        red = new RedNeuronal(numInput, numHidden, numOutput);
+       
+        EntrenarRed();
+    }
+
+    //Se entrena la red neural con los valores del entrenamiento predeterminado
+    private void EntrenarRed()
+    {
+        float error = 1;
+        int epoch = 0;
+
+        //Se reentrena numerosas veces ya que los primeros pesos son aleatorios y tienes que asegurar un primer entrenamiento optimo
+        while ((error > 0.05f) && (epoch < 50000))
+        {
+            error = 0;
+            epoch++;
+            for (int i = 0; i < entrenamiento.GetLength(0); i++)
+            {
+                //Entrenar la red
+                for (int j = 0; j < numInput; j++)
+                {
+                    red.SetInput(j, entrenamiento[i, j]);
+                }
+
+                //Ajusta los outputs deseados
+                for (int j = numInput; j < numInput + numOutput; j++)
+                    red.SetOutputDeseado(j - numInput, entrenamiento[i, j]);
+
+                //Calculo de los valores de las neuronas
+                red.FeedForward();
+                error += red.CalcularError();
+                red.BackPropagation();
+            }
+            error /= entrenamiento.GetLength(0);
+            // VER COMO EVOLUCIONA EL ERROR A MEDIDA QUE AVANZAN LOS EPOCHS
+        }
+    }
+
+    //Vuelve a entrenar la neurona, con los nuevos valores recibidos (asi se fomenta el aprendizaje)
+    public void ReentrenarRed(float[] inputs, float[] output)
+    {
+        float error = 1;
+        int epoch = 0;
+
+        while ((error > 0.1f) && (epoch < 1000))
+        {
+            epoch++;
+            //Entrenar la red
+            for (int j = 0; j < numInput; j++)
+                red.SetInput(j, inputs[j]);
+
+            //Ajusta los outputs deseados
+            for (int j = 0; j < numOutput; j++)
+                red.SetOutputDeseado(j, output[j]);
+
+            //Calculo de los valores de las neuronas
+            red.FeedForward();
+            error = red.CalcularError();
+            red.BackPropagation();
+            // VER COMO EVOLUCIONA EL ERROR A MEDIDA QUE AVANZAN LOS EPOCHS
+        }
+    }
+
+    //Con los inputs recibidos, te devuelve el output de la accion a realizar
+    public byte ConsultarAccion(float[] inputs)
+    {
+        for (int i = 0; i < inputs.Length; i++)
+        {
+            red.SetInput(i, inputs[i]);
+        }
+        red.FeedForward();
+        return (byte)red.GetMaxOutputId();
+    }
+
+    //Getter de los outputs
+    public float GetOutput(int i)
+    {
+        return red.GetOutput(i);
+    }
+
+    //Getter de la id del mejor output
+    public int GetMaxOutputId()
+    {
+        return red.GetMaxOutputId();
+    }
+}
